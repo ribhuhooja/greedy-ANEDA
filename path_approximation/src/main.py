3ce4ed2515023a5fdd3dea0a97f63db7ad6ab9b2
chaupham <chau.datascience@gmail.com>
2021-12-07 02:16:33 -0500

    setup experiments

--- path_approximation/src/main.py
+++ path_approximation/src/main.py
@@ -1,6 +1,7 @@
 from datasets_generator import create_train_val_test_sets
 from data_helper import read_yaml
-from utils import make_log_folder
+from utils import make_log_folder, generate_config_list
+
 from train_neural_net import train_neural_net
 
 if __name__ == '__main__':
@@ -10,9 +11,16 @@ if __name__ == '__main__':
     ## Make a log folder to log the experiments
     make_log_folder(log_folder_name=data_generator_config["log_path"])
 
-    ## Make a list of configs, we'll be running the model for each config
-    # config_list = generate_config_list(data_generator_config)
+    # Make a list of configs, we'll be running the model for each config
+    config_list = generate_config_list(data_generator_config)
 
     dataset = create_train_val_test_sets(data_generator_config)
 
-    train_neural_net(dataset)
+    for i, config in enumerate(config_list):
+        dataset = create_train_val_test_sets(config)
+        train_neural_net(dataset)
+        print("\n------------\n")
+
+
+
+
